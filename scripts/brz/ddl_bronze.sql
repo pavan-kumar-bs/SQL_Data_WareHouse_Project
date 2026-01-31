890febe702a7c4ab22aa8dd186a2ecfaec853dcd
pavan-kumar-bs <pavankumarbs321@gmail.com>
2026-01-31 16:16:39 +0530

    Update ddl_bronze.sql

--- scripts/brz/ddl_bronze.sql
+++ scripts/brz/ddl_bronze.sql
@@ -1,1 +1,19 @@
 
+--Creating DataWarehouse Database and setting up three schemas within the Database "bronze, silver and gold"
+
+USE MASTER;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
